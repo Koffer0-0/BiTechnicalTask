fe998a4f12bec6e79c135e5ee373322b14222344
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2025-07-30 18:46:37 +0500

    feat: create basic catalog page

--- bi-app-nuxt/app/components/ProductList.vue
+++ bi-app-nuxt/app/components/ProductList.vue
@@ -61,6 +61,38 @@ const products = ref([
 </script>
 
 <template>
+  <section>
+    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+      <header>
+        <h2 class="text-xl font-bold text-gray-900 sm:text-3xl">Catalog</h2>
+
+        <p class="mt-4 max-w-md text-gray-500">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque praesentium cumque iure
+          dicta incidunt est ipsam, officia dolor fugit natus?
+        </p>
+      </header>
+
+      <div class="mt-8 block lg:hidden">
+        <button
+            class="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600"
+        >
+          <span class="text-sm font-medium"> Filters & Sorting </span>
+
+          <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-4 rtl:rotate-180"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+        </button>
+      </div>
+
+    </div>
+  </section>
 </template>
 
 <style scoped>
