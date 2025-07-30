54afe40b6b4ae3882ed4792b6b938cbf7d0fb49e
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2025-07-30 18:47:37 +0500

    Add product card UI and add-to-cart handler
    Introduces a ProductCard component that displays product image, name, price, and a button to add the product to the cart. The product is passed as a prop,

--- bi-app-nuxt/app/components/ProductCard.vue
+++ bi-app-nuxt/app/components/ProductCard.vue
@@ -1,7 +1,34 @@
 <script setup lang="ts">
+defineProps({
+  product: {
+    type: Object,
+    required: true
+  }
+})
+
 </script>
 
 <template>
+  <a href="#" class="group block overflow-hidden">
+    <img
+        :src="product.imageURL"
+        :alt="product.imageAlt"
+        class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+    />
+
+    <div class="relative bg-white p-3">
+      <h3
+          class="text-sm text-gray-700 group-hover:underline group-hover:underline-offset-4"
+      >
+        {{ product.name }}
+      </h3>
+
+      <p class="mt-1">
+        <span class="tracking-wider text-gray-900 text-lg"> {{ product.price }} KZT </span>
+      </p>
+
+    </div>
+  </a>
 </template>
 
 <style scoped>
