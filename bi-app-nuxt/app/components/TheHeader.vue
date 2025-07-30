22ac3a2ff4f8c8f8360648457f4da2855233af32
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2025-07-30 18:27:47 +0500

    Create TheHeader.vue

---
+++ bi-app-nuxt/app/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="bg-white">
+    <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 items-center justify-between">
+        <div class="flex-1 md:flex md:items-center md:gap-12">
+          <a class="block text-teal-600" href="#">
+            <span>Home</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
