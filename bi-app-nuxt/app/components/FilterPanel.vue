73746d502bd6cbc5202e21697c1b05712f0e91d5
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2025-07-31 17:31:47 +0500

    Refactor search input into reusable SearchBar component
    Moved the search input markup from FilterPanel.vue to a new SearchBar.vue component and replaced the inline search input in FilterPanel.vue with the SearchBar component. This improves code reuse and maintainability.

--- bi-app-nuxt/app/components/FilterPanel.vue
+++ bi-app-nuxt/app/components/FilterPanel.vue
@@ -70,27 +70,7 @@ const resetFilters = () => {
 
 <template>
   <div class="hidden space-y-4 lg:block">
-    <label
-        for="productSearch"
-        class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-    >
-      <input
-          type="email"
-          id="productSearch"
-          placeholder="search"
-          v-model="searchQuery"
-          class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-      />
-
-      <span class="flex absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-              </svg>
-               <span class="ml-2">
-                Search Product
-              </span>
-            </span>
-    </label>
+    <SearchBar v-model="searchQuery"/>
     <div>
       <label for="SortBy" class="block text-xs font-medium text-gray-700"> Sort By </label>
 
