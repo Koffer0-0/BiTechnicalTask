bca3b502a1b23973e351fa0eea7899f606d77cd8
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2025-07-31 17:14:20 +0500

    Rename and implement product detail page
    Deleted [ProductPage].vue and added product/[id].vue with logic to fetch product details by ID and handle adding products to cart. This improves route structure and functionality for individual product pages.

---
+++ bi-app-nuxt/app/pages/product/[id].vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+const { handleFetchProductById } = useProduct()
+const { addToCart } = useCart();
+const product = ref({})
+
+onMounted(() => {
+  const productId = route.params.id;
+  productDetails(productId)
+})
+
+const productDetails = async (productId) => {
+  const result = await handleFetchProductById(productId)
+  product.value = result
+}
+
+const handleAddToCart = (product) => {
+  addToCart(product)
+}
+</script>
+
+<template>
+  <ProductDetail :product="product" @updateCart="handleAddToCart"/>
+</template>
+
+<style scoped>
+
+</style>
